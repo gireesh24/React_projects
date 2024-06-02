a0b9f74aee9ab77ef3afe2f9da7f6419a008b672
gireesh24 <gireeshvysyaraju@gmail.com>
2024-06-02 17:23:38 +0530

    image

--- Imdb/src/Components/Navbar.jsx
+++ Imdb/src/Components/Navbar.jsx
@@ -1,18 +1,20 @@
 // import React from 'react'
 
 import { Link } from "react-router-dom"
-import MovieLogo from './movie.jpg';
-
+import MovieLogo from "./assets/movie.jpg";
+// import viteLogo from '/vite.svg'
 
 function Navbar() {
   return (
     <>
+    <div className="flex space-x-8 items-center pl-3 py-4">
     <Link to="/">
-        <img src= {MovieLogo} />
+        <img className="w-[50px] h-[40px]" src= {MovieLogo} />
     </Link>
-    <Link to="/">Movies</Link>
-    <Link to="/Watchlist">WatchList</Link>
+    <Link className="text-blue-500 text-3xl font-bold" to="/">Movies</Link>
+    <Link className="text-blue-500 text-3xl font-bold" to="/Watchlist">WatchList</Link>
     <div>hello</div>
+    </div>
     </>
 )   
 }
--- Imdb/src/ImdbApp.jsx
+++ Imdb/src/ImdbApp.jsx
@@ -1,9 +1,9 @@
 // import React from 'react'
 
 import { Route, Routes } from "react-router-dom"
-import Navbar from "../../Imdb Clone/src/Components/Navbar"
 import Home from "./Pages/Home"
 import WatchList from "./Components/WatchList"
+import Navbar from "./Components/Navbar"
 
 
 function ImdbApp() {
